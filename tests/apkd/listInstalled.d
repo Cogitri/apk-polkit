1bcfa1011e9a751d7a11b414094c22b445e8c1cc
Rasmus Thomsen <oss@cogitri.dev>
2020-03-29 13:22:17 +0200

    test(apkd): fix listInstalled tests, we now throw if no packages are found

--- tests/apkd/listInstalled.d
+++ tests/apkd/listInstalled.d
@@ -20,6 +20,7 @@
 module tests.apkd.listInstalled;
 
 import apkd.ApkDataBase;
+import apkd.exceptions;
 import std.exception;
 import std.format;
 import tests.apkd.testlib;
@@ -28,10 +29,6 @@ int main(string[] args)
 {
     auto testHelper = TestHelper(args, "listInstalled");
     auto database = new ApkDataBase(testHelper.apkRootDir, testHelper.repoDir);
-    auto installedPkgs = database.getInstalledPackages();
-    immutable auto expectedInstalledPkgs = 0;
-    enforce(installedPkgs.length == expectedInstalledPkgs,
-            format("Expected %s installed packages, got %s",
-                expectedInstalledPkgs, installedPkgs.length));
+    assertThrown!ApkListException(database.getInstalledPackages());
     return 0;
 }
