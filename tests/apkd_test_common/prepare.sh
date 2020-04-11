34919e78eb8e5216e3075122977ba963147e025b
Rasmus Thomsen <oss@cogitri.dev>
2020-04-11 15:14:54 +0200

    test(apkd_test_common): add generated repo to repo list

--- tests/apkd_test_common/prepare.sh
+++ tests/apkd_test_common/prepare.sh
@@ -18,6 +18,8 @@ cp -r $(dirname "$0")/repo "$2"/abuilds
 
 cd "$2"
 
+echo "$2"/abuilds > "$1"/etc/apk/repositories
+
 for x in abuilds/*/APKBUILD; do
     pushd ${x%/*}
     APK="apk --allow-untrusted --root $1" SUDO_APK="abuild-apk --root $1" REPODEST="$2" abuild -F clean unpack prepare build rootpkg update_abuildrepo_index
