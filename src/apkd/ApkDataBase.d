0568b05d437c84d73736225d6e337f91cd095a72
Rasmus Thomsen <oss@cogitri.dev>
2020-04-20 17:26:37 +0200

    Revert "fix(apkd): pass more flags to the solver"
    This reverts commit 81a797f93fcdbdc17c5be1769e0ed422b9e62f8d.
    
    This broke upgrading packages

--- src/apkd/ApkDataBase.d
+++ src/apkd/ApkDataBase.d
@@ -214,9 +214,9 @@ struct ApkDataBase
     *   Throws an ApkSolverException if the solver can't figure out a way to solve
     *   the upgrade, e.g. due to conflicts.
     */
-    void upgradeAllPackages(ushort solverFlags = APK_SOLVERF_AVAILABLE)
+    void upgradeAllPackages(ushort solverFlags = 0)
     {
-        auto changeset = this.getAllUpgradeChangeset(solverFlags);
+        auto changeset = this.getAllUpgradeChangeset();
         const auto solverRes = apk_solver_commit_changeset(&this.db, &changeset, this.db.world);
         enforce!ApkSolverException(solverRes == 0,
                 format("Couldn't upgrade packages due to error '%s%",
@@ -236,8 +236,7 @@ struct ApkDataBase
     *   Throws an ApkDatabaseCommitException if commiting the changes to the database fails, e.g.
     *   due to missing permissions, a conflict, etc.
     */
-    void upgradePackage(string[] pkgnames,
-            ushort solverFlags = APK_SOLVERF_IGNORE_UPGRADE | APK_SOLVERF_AVAILABLE)
+    void upgradePackage(string[] pkgnames, ushort solverFlags = APK_SOLVERF_IGNORE_UPGRADE)
     {
         apk_dependency_array* worldCopy = null;
         scope (exit)
@@ -250,8 +249,7 @@ struct ApkDataBase
         {
             auto apkDep = this.packageNameToApkDependency(pkgname);
             apk_deps_add(&worldCopy, &apkDep);
-            apk_solver_set_name_flags(apkDep.name,
-                    APK_SOLVERF_UPGRADE | solverFlags, APK_SOLVERF_UPGRADE | solverFlags);
+            apk_solver_set_name_flags(apkDep.name, APK_SOLVERF_UPGRADE, APK_SOLVERF_UPGRADE);
         }
 
         const auto solverCommitRes = apk_solver_commit(&this.db, 0, worldCopy);
@@ -273,7 +271,7 @@ struct ApkDataBase
     *   Throws an ApkDatabaseCommitException if commiting the changes to the database fails, e.g.
     *   due to missing permissions, a conflict, etc.
     */
-    void addPackage(string[] pkgnames, ushort solverFlags = APK_SOLVERF_AVAILABLE)
+    void addPackage(string[] pkgnames, ushort solverFlags = 0)
     {
         apk_dependency_array* worldCopy = null;
         scope (exit)
@@ -287,8 +285,7 @@ struct ApkDataBase
             auto dep = packageNameToApkDependency(pkgname);
 
             apk_deps_add(&worldCopy, &dep);
-            apk_solver_set_name_flags(dep.name, APK_SOLVERF_IGNORE_UPGRADE | solverFlags,
-                    APK_SOLVERF_IGNORE_UPGRADE | solverFlags);
+            apk_solver_set_name_flags(dep.name, solverFlags, solverFlags);
         }
 
         const auto solverCommitErrorCount = apk_solver_commit(&this.db, solverFlags, worldCopy);
