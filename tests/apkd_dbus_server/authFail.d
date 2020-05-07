116407196cc41dbf94962a7b2f7c67e1652140b3
Rasmus Thomsen <oss@cogitri.dev>
2020-05-07 18:36:02 +0200

    test(apkd_dbus_server): add authFailed test

---
+++ tests/apkd_dbus_server/authFail.d
@@ -0,0 +1,79 @@
+/*
+    Copyright (c) 2020 Rasmus Thomsen <oss@cogitri.dev>
+
+    This file is part of apk-polkit (see https://github.com/Cogitri/apk-polkit).
+
+    apk-polkit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    apk-polkit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with apk-polkit.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+module tests.apkd_dbus_server.authFail;
+
+import core.stdc.stdlib : exit;
+import tests.apkd_test_common.testlib;
+import tests.apkd_test_common.apkd_dbus_client;
+import gio.c.types : GDBusConnection, BusType, GDBusProxyFlags;
+import glib.c.types : GError;
+import glib.GException;
+import glib.Variant;
+import std.exception;
+import std.conv : to;
+import std.file : exists;
+import std.format : format;
+import std.path : buildPath;
+import std.process : execute;
+import std.string : strip, toStringz;
+
+extern (C) void onNameAppeared(GDBusConnection* connection, const(char)* name,
+        const(char)* nameOwner, void* userData)
+{
+    auto testHelper = cast(TestHelper*) userData;
+    GError* error;
+
+    scope (exit)
+    {
+        testHelper.cleanup();
+    }
+
+    auto apkdHelper = apkd_helper_proxy_new_for_bus_sync(BusType.SYSTEM, GDBusProxyFlags.NONE,
+            "dev.Cogitri.apkPolkit.Helper".toStringz(),
+            "/dev/Cogitri/apkPolkit/Helper".toStringz(), null, null);
+    apkd_helper_set_allow_untrusted_repos(apkdHelper, true);
+    apkd_helper_set_root(apkdHelper, testHelper.apkRootDir.toStringz());
+    auto pkgs = ["test-a".toStringz(), "test-e".toStringz(), null];
+    assert(!apkd_helper_call_add_packages_sync(apkdHelper, pkgs.ptr, null,
+            &error), error.message.to!string);
+
+    assert(!buildPath(testHelper.apkRootDir, "usr", "bin", "test-a").exists());
+
+    assert(error.message.to!string == "GDBus.Error:org.freedesktop.DBus.Error.AccessDenied: Authorization for operationaddPackagesfor has failed for user!");
+
+    testHelper.cleanup();
+    exit(0);
+}
+
+extern extern (C) __gshared bool rt_trapExceptions;
+extern extern (C) int _d_run_main(int, char**, void*);
+
+extern (C) int main(int argc, char** argv)
+{
+    rt_trapExceptions = false;
+    return _d_run_main(argc, argv, &_main);
+}
+
+int _main(string[] args)
+{
+    auto testHelper = TestHelper(args, "dbusServerAuthFail");
+    setupDbusServer(args[3], [], &onNameAppeared, &nameVanishedCallback, &testHelper);
+    return 0;
+}
